b4dba2618f0172f46b526f8a3c2bde5cb942a0f1
Mateo <mateo10velasquez@hotmail.com>
2019-11-03 16:56:57 -0500

    Carpeta Eliminada

--- Algoritmo Genetico/fitness.m
+++
@@ -1,9 +0,0 @@
-
-function [Result] = fitness(Array_ind)
-global Mtx_coeff Mtx_result
-%calcula el fitness de una fila                                                       
-Aux1 = Mtx_coeff*transpose(Array_ind);
-Aux2 = abs(Mtx_result - Aux1);
-Result = sum(Aux2);
-
-end
